aebb1ebd14293ce949f784db46be9981cfb65a18
user <alimmamadovxurshed04@gmail.com>
2025-05-24 17:38:50 +0500

    Optimized for Performance

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -3,15 +3,12 @@ import Link from "next/link";
 export default function HeroSection() {
   return (
     <div className="relative overflow-hidden bg-gray-900 -mt-16">
-      {/* Background gradient effects */}
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute -top-40 -right-40 h-96 w-96 rounded-full bg-blue-700 opacity-20 blur-3xl"></div>
         <div className="absolute -bottom-40 -left-40 h-96 w-96 rounded-full bg-green-600 opacity-20 blur-3xl"></div>
         <div className="absolute -top-40 -left-40 h-96 w-96 rounded-full bg-cyan-500 opacity-20 blur-3xl"></div>
         <div className="absolute -bottom-40 -right-40 h-96 w-96 rounded-full bg-blue-400 opacity-20 blur-3xl"></div>
       </div>
-
-      {/* Optional hero image overlay */}
       <div className="absolute inset-0 pointer-events-none hidden lg:block z-20">
   <div
     className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-[400px] w-[400px] rounded-full opacity-40 blur-[100px] z-30"
@@ -48,22 +45,21 @@ export default function HeroSection() {
           </Link>
         </div>
 
-        {/* Floating repair previews */}
         <div className="mt-16 relative">
           <div className="relative mx-auto w-[400px] flex items-center justify-center">
             <div
               className="rounded-lg shadow-2xl overflow-hidden bg-cover w-[200px] h-[250px] z-10 transition duration-700 hover:scale-105 outline-0 outline-offset-0 hover:outline-8 outline-blue-500 hover:outline-offset-[10px]"
-              style={{ backgroundImage: "url('/hardware1.png')" }}
+              style={{ backgroundImage: "url('/hardware1.webp')" }}
             ></div>
 
             <div
               className="absolute -left-16 top-1/8 transform -rotate-6 opacity-90 shadow-xl rounded-lg overflow-hidden hidden lg:block w-[200px] h-[250px] bg-cover bg-right transition duration-700 hover:scale-105 outline-0 outline-offset-0 hover:outline-8 outline-green-500 hover:outline-offset-[10px] hover:z-20"
-              style={{ backgroundImage: "url('/hardware2.jpg')" }}
+              style={{ backgroundImage: "url('/hardware2.webp')" }}
             ></div>
 
             <div
               className="absolute -right-16 top-1/8 transform rotate-6 opacity-90 shadow-xl rounded-lg overflow-hidden hidden lg:block w-[200px] h-[250px] bg-cover bg-center transition duration-700 hover:scale-105 outline-0 outline-offset-0 hover:outline-8 outline-cyan-500 hover:outline-offset-[10px] hover:z-20"
-              style={{ backgroundImage: "url('/hardware3.jpg')" }}
+              style={{ backgroundImage: "url('/hardware3.webp')" }}
             ></div>
           </div>
         </div>
